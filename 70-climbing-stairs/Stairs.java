c4f3d191315abefc6983a111661f2328ad3d1f67
Sujal Kumar Jaiswal <141017209+SKJ57@users.noreply.github.com>
2025-09-19 15:31:09 +0530

    Add files via upload
    using recursion

---
+++ 70-climbing-stairs/Stairs.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Stairs {
+    public static int climbStairs(int n) {
+        if(n==1 || n==2){
+            return n;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = climbStairs(n);
+        System.out.println(res);
+    }
+}
