92abcf56d63a4f426fbfa3345ce5ab7dfae641c6
Sujal Kumar Jaiswal <141017209+SKJ57@users.noreply.github.com>
2025-10-01 14:17:20 +0530

    Time: 2 ms (94.17%) | Memory: 46.8 MB (52.34%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -3,17 +3,8 @@ class Solution {
         int[] ans = new int[n+1];
 
         for(int i=0; i<=n; i++){
-            String bin = Integer.toBinaryString(i);
-            int sum = binAdd(bin);
-            ans[i] = sum;
+           ans[i] = ans[i/2] + (i&1);
         }
         return ans;
-    }
-    public int binAdd(String b){
-        int sum=0;
-        for(char c: b.toCharArray()){
-            sum += c - '0';
-        }
-        return sum;
     }
 }
