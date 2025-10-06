224c3b9de78cf1ed4da9cb98b3b3b2ac1574ba63
Sujal Kumar Jaiswal <141017209+SKJ57@users.noreply.github.com>
2025-10-06 23:23:34 +0530

    Time: 7 ms (72.30%) | Memory: 45.2 MB (52.02%) - LeetSync

--- 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.java
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.java
@@ -1,14 +1,13 @@
 class Solution {
     public int smallestAbsent(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        int sum = 0, ans=0;
+        int sum = 0;
         for(int num: nums){
             if(num>0) set.add(num);
             sum+=num;
         }
         int avg = sum/nums.length;
-        if(avg>0) ans = avg+1;
-        else ans = 1;
+        int ans = (avg>0)? avg+1 : 1;
         while(set.contains(ans)){
             ans++;
         }
