e9304056dda97e04e048534dc936ca2a55ccefa6
Sujal Kumar Jaiswal <141017209+SKJ57@users.noreply.github.com>
2025-10-17 10:27:28 +0530

    Time: 33 ms (26.79%) | Memory: 45.8 MB (65.60%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,16 +1,14 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
         int n = candyType.length;
-        Arrays.sort(candyType);
-        int count = 1;
-        for(int i=1; i<n; i++){
-            int c = candyType[i];
-            if(c!=candyType[i-1]){
-                count++;
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: candyType){
+            set.add(num);
         }
+        int s = set.size();
         int avl = n/2;
-        if(count>=avl) return avl;
-        else return count;
+
+        if(s>=avl) return avl;
+        else return s;
     }
 }
